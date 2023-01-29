8484d1502fdc5066ce67c8f1b589b5ff8aa2904b
Fedor Yakouchyk <fedar77@outlook.com>
2023-01-30 00:26:51 +0300

    Доработанно задание 3

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,5 +1,22 @@
-﻿System.Console.Write("Введите размер массива: ");
-int A = Convert.ToInt32(Console.ReadLine());
+﻿
+int A = inputNumber("Введите размер массива: ");
+
+int inputNumber(string str)
+{
+    int number;
+    string text;
+    while (true)
+    {
+        Console.Write(str);
+        text = Console.ReadLine();
+        if (int.TryParse(text, out number))
+        {
+            break;
+        }
+        System.Console.WriteLine("Введено некорректное число, попробуйте снова");
+    }
+    return number;
+}
 
 int[] randomMas = new int[A];
 Random rand = new Random();
@@ -7,4 +24,4 @@ for (int i = 0; i < randomMas.Length; i++)
 {
     randomMas[i] = rand.Next(0, 1000);
 }
-System.Console.WriteLine( string.Join(", ", randomMas));
+System.Console.WriteLine($"[{string.Join(", ", randomMas)}]");
