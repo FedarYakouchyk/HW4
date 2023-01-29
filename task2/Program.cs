7ee757d46b20ccc1c616c4006322ad015e857030
Fedor Yakouchyk <fedar77@outlook.com>
2023-01-29 23:28:34 +0300

    Добавленно задание 2

---
+++ task2/Program.cs
@@ -0,0 +1,10 @@
+﻿System.Console.Write("Введите размер массива: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+int[] randomMas = new int[A];
+Random rand = new Random();
+for (int i = 0; i < randomMas.Length; i++)
+{
+    randomMas[i] = rand.Next(0, 1000);
+}
+System.Console.WriteLine( string.Join(", ", randomMas));
