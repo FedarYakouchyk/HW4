ab7e58a3a5ab61df7128198386a9c89cc7e48ae1
Fedor Yakouchyk <fedar77@outlook.com>
2023-01-30 00:05:06 +0300

    Добавленно задание 3

---
+++ task3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите любое число: ");
+string? num = Console.ReadLine();
+int Num = Convert.ToInt32(Convert.ToString(num));
+int sum (int number)
+{
+    int rez = 0;
+    for (int i = 1; i <= num!.Length; i++)
+    {
+        int ost = Num % 10;
+        rez = rez + ost;
+        Num = Num / 10;
+    }
+    return rez;
+}
+System.Console.WriteLine($"Сумма цифр составляющих число {Num} = {sum(Num)}");
